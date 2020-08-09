25103b111071c290665e4d89d6780425325c9f72
SangMin <tkdals9048@naver.com>
2020-08-10 01:18:38 +0900

    Day - 01

---
+++ 2.끝말잇기/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스 : production
+    devtool: 'eval', // hidden-source-map
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR'],
+                        },
+                        debug: true,
+                    }], 
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-hot-loader/babel'
+                ]
+            }
+        }],
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist/'
+    }, // 출력
+};
